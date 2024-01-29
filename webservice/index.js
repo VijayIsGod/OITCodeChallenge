96e6c5ad21174b891d588551cb138d510e0bfe40
VijayIsGod <71033503+VijayIsGod@users.noreply.github.com>
2024-01-28 21:31:19 -0700

    Finished back-end

--- webservice/index.js
+++ webservice/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
+const tmdb = require('./services/tmdb')
 const app = express();
 
 // Create a route for the root URL
 app.get('/',(req,res) => {
     res.send('Sorry wrong endpoint, please go to /movies')
 })
-app.get('/movies?search={title}', (req, res) => {
-    res.send('Hello World!');
+app.get('/movies', async (req, res) => {
+    res.send(await tmdb.getMovies(req.query?.search));// query contains search string which is the title.
 });
-
 // Start the server
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
--- webservice/services/tmdb.js
+++ webservice/services/tmdb.js
@@ -9,7 +9,26 @@ const options = {
     }
 };
 
-fetch(url, options)
-    .then(res => res.json())
-    .then(json => console.log(json))
-    .catch(err => console.error('error:' + err));
+exports.getMovies = async function(title) {
+    try {
+        const results = []
+        const posterBaseUrl = 'https://image.tmdb.org/t/p/w500'
+        const movies = await fetch(url.replace('{title}',title), options)
+                            .then(res => res.json())
+                            .catch(err => console.error('error:' + err));
+        
+        for(let i = 0; i < movies.results.length; i++) {
+            const movie = movies.results[i]
+            const result = {
+                movie_id : movie.id,
+                title : movie.title,
+                poster_image_url : movie.poster_path ? posterBaseUrl + movie.poster_path : '',
+                popularity_summary : `${movie.popularity} out of ${movie.vote_count}` 
+            }
+            results.push(result)
+        }
+        return results
+    } catch (error) {
+        return 'It Failed. Try again with a different search parameter'
+    }
+}
